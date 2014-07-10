782b2dffdd38c7036342876445204fa572b81b7b
roscodog614 <waltz.dan@outlook.com>
2014-07-10 16:29:45 -0400

    Added the controllers, route, service, and completed most of the final page (prodPerfDetails.html)

--- app/js/app.js
+++ app/js/app.js
@@ -3,41 +3,49 @@
 
 // Declare app level module which depends on filters, and services
 angular.module
-(
-    'myApp',
-    [
-        'ngRoute',
-        'myApp.filters',
-        'myApp.services',
-        'myApp.directives',
-        'myApp.controllers'
-    ]
-)
-.config
-(
-    [
-        '$routeProvider',
-        function ($routeProvider)
-        {
-            $routeProvider.when
-            (
-                '/home',
-                {
-                    templateUrl: 'partials/home.html',
-                    controller: 'ngcMain'
-                }
-            );
+    (
+        'myApp',
+        [
+            'ngRoute',
+            'myApp.filters',
+            'myApp.services',
+            'myApp.directives',
+            'myApp.controllers'
+        ]
+    )
+    .config
+    (
+        [
+            '$routeProvider',
+            function($routeProvider) {
+                $routeProvider.when
+                (
+                    '/home',
+                    {
+                        templateUrl: 'partials/home.html',
+                        controller: 'ngcMain'
+                    }
+                );
 
-            $routeProvider.when
-            (
-                '/prodPerfSumm',
-                {
-                    templateUrl: 'partials/prodPerfSumm.html',
-                    controller: 'ngcProdPerfSumm'
-                }
-            );
+                $routeProvider.when
+                (
+                    '/prodPerfSumm',
+                    {
+                        templateUrl: 'partials/prodPerfSumm.html',
+                        controller: 'ngcProdPerfSumm'
+                    }
+                );
 
-            $routeProvider.otherwise({ redirectTo: '/home' });
-        }
-    ]
-);
+                $routeProvider.when
+                (
+                    '/prodPerfDetails',
+                    {
+                        templateUrl: 'partials/prodPerfDetails.html',
+                        controller: 'ngcProdPerfDetails'
+                    }
+                );
+
+                $routeProvider.otherwise({ redirectTo: '/home' });
+            }
+        ]
+    );
